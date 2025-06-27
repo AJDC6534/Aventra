c93a89849fc7cac3b96b4dba5f8200c0000989b0
AJDC6534 <sinon6534@gmail.com>
2025-06-28 02:29:58 +0400

    commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -75,7 +75,7 @@ export default {
       this.error = ''
       
       try {
-        const response = await fetch('${import.meta.env.VITE_API_BASE_URL}/api/auth/login', {
+        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
