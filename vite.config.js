67e09fd8f6945fac2962443a17b4d13d5f2eaf19
AJDC6534 <sinon6534@gmail.com>
2025-07-09 01:14:24 +0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/Aventra/',
   plugins: [vue()],
-  server: {
-    port: 3000,
-    proxy: {
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
       '/api': {
         target: 'https://aventra-backend.onrender.com',
         changeOrigin: true,
       },
     },
-  },
-})
+  })
