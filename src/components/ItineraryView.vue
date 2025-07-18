b49047b2d68663af2124239755f7f98fea0bf21e
AJDC6534 <sinon6534@gmail.com>
2025-07-18 17:24:09 +0400

    commit

--- src/components/ItineraryView.vue
+++ src/components/ItineraryView.vue
@@ -1201,7 +1201,7 @@ Generate ${currentValues.pace === 'relaxed' ? '2-3' : currentValues.pace === 'ac
       
       if (newItineraryData && newItineraryData.days) {
         // Now update the existing itinerary with the new data
-        const updateResponse = await fetch(`${API_BASE_URL}/api/itineraries/${this.itinerary._id}`, {
+        const updateResponse = await fetch(`${API_BASE_URL}/api/generate-itineraries/${this.itinerary._id}`, {
           method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
